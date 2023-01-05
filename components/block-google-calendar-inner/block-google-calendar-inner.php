7450f56c047e2b264d448c82c3bbef5bac6f0f0c
Janne Hyyryläinen <janne.hyyrylainen@into-digital.fi>
2023-01-05 12:18:27 +0200

    changes to google calendar embed

--- components/block-google-calendar-inner/block-google-calendar-inner.php
+++ components/block-google-calendar-inner/block-google-calendar-inner.php
@@ -9,15 +9,27 @@
  * @version 1.0
  */
 
-$embed_code = get_field('embed_code');
-$embed_code_sanitized = '';
-
-if(!empty($embed_code)) {
-	$embed_code_sanitized = strip_tags($embed_code, '<iframe>');
-
-	$embed_code_sanitized = str_replace("src=\"", "data-src=\"", $embed_code_sanitized);
-    $embed_code_sanitized = str_replace("<iframe", "<iframe data-name=\"third_party\"", $embed_code_sanitized);
-}
+ $embed_code = get_field('embed_code');
+ $showTitle = get_field('title') === false ? '&showTitle=0' : '';
+ $showNavBtns = get_field('nav_btns') === false ? '&showNav=0' : '';
+ $showDate = get_field('date') === false ? '&showDate=0' : '';
+ $showPrintIcon = get_field('print_icon') === false ? '&showPrint=0' : '';
+ $showTabs = get_field('tabs') === false ? '&showTabs=0' : '';
+ $showCalendarList = get_field('calendar_list') === false ? '&showCalendars=0' : '';
+ $showTimeZone = get_field('time_zone') === false ? '&showTz=0' : '';
+ 
+ $embed_url = $embed_code . $showTitle . $showNavBtns . $showDate . $showPrintIcon . $showTabs .  $showCalendarList . $showTimeZone;
+ 
+ $valid_host = false;
+ $built_iframe = '';
+ 
+ if(!empty($embed_code)) {
+	 if(!is_wp_error(parse_url($embed_code))) {
+		 $url_obj = parse_url($embed_code);
+		 $valid_host = $url_obj['host'] === 'calendar.google.com' ? true : false;
+		 $built_iframe = '<iframe data-src="' . $embed_url . '" data-name="third_party" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>';
+	 }
+ }
 ?>
 
 <?php 
@@ -35,7 +47,9 @@ endif;
 	if(is_admin()) {
 		echo '<div class="admin-message">' . __('Tässä näytetään Google kalenteri', 'ID_admin') . '</div>'; 
 	} else {
-		echo $embed_code_sanitized; 
+		if(!empty($embed_code)) {
+			echo $built_iframe; 
+		}
 	}
 	?>
 </div>
